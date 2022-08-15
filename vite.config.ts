ab9f480fe3f9ce20dcf6586841f405a366bc60a0
renshuo <renshuoo@qq.com>
2022-08-15 17:47:49 +0800

    更新pnpm发布配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/cf/cesiumFlyer.ts"),
+      name: 'cesium-flyer',
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        'cesium',
+      ],
+    }
+  }
 })
