6792f37121d7d2062141d66a98f6f411ed1b184f
renshuo <renshuoo@qq.com>
2022-08-15 11:31:07 +0800

    增加cesium依赖

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="position: relative">
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+window.CESIUM_BASE_URL = "/Cesium";
+import * as Cesium from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNWY5YzhhMS05ZmYxLTQ5NzgtOTcwNC0zZmViNGFjZjc4ODEiLCJpZCI6ODU0MjMsImlhdCI6MTY0Njk4ODA1NX0.4-plF_5ZfEMMpHqJyefkDCFC8JWkFw39s3yKVcNg55c";
+  Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+    100,
+    30,
+    110,
+    40
+  );
+  let viewer = new Cesium.Viewer("mapContainer", {
+    infoBox: false, //是否显示信息框
+    selectionIndicator: false, //是否显示选取指示器组件
+    timeline: false, //是否显示时间轴
+    animation: false, //是否创建动画小器件，左下角仪表
+  });
+});
+</script>
+
+<style></style>
